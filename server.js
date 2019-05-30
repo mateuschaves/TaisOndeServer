db8daa9838320909a7138bcfbe983ff093847e7d
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-05-29 21:41:25 -0300

    dependencies and .gitignore

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Servidou iniciou na porta 3000 !');
+});
 
 io.on('connection', function(socket){
     console.log(socket.id);
